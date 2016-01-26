c7d509b4007067ee58cf08ea353c8baf62b1ce0a
Aleks Angelov <aleks_angelov@mail.com>
2016-01-27 00:34:38 +0200

    Finished Project

---
+++ GameCloud/GameLibrary.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace GameCloud
+{
+    /// <summary>
+    ///     Interaction logic for GameLibrary.xaml
+    /// </summary>
+    public partial class GameLibrary
+    {
+        public GameLibrary()
+        {
+            InitializeComponent();
+        }
+
+        private void gameButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("cmd", "/C mstsc /v:169.254.173.193 /f");
+            //netsh wlan set hostednetwork mode=allow ssid=gamecloud key=12345678
+            //netsh wlan start hostednetwork
+        }
+    }
+}
---
+++ GameCloud/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace GameCloud
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var librarywindow = (GameLibrary) Application.LoadComponent(new Uri("GameLibrary.xaml", UriKind.Relative));
+            librarywindow.Show();
+            Close();
+        }
+    }
+}
